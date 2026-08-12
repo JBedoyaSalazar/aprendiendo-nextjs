92207b572f79443cd5af503ec3a475a00b8b22e9
Jefred Bedoya <jefred03salazar@gmail.com>
2026-08-11 20:11:05 -0500

    feat: update layout with navigation links and add category layout component

---
+++ src/app/store/[category]/layout.tsx
@@ -0,0 +1,10 @@
+import '../../globals.css'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <main>Navegacion de las categorias</main>
+            {children}
+        </div>
+    )
+}
