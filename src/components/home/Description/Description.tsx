4b12f8cf521f37368a5712fcbaa90773e0a37c9c
Jefred Bedoya <jefred03salazar@gmail.com>
2026-08-13 17:29:56 -0500

    refactor: restructure Description component for improved image handling and styling

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -4,20 +4,21 @@ import styles from "./Description.module.sass";
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <Image
-        src="/images/description.jpeg"
-        alt="products marketplace"
-        width={500}
-        height={300}
-        loading="eager"
-        className={styles.Description__image}
-      />
+      <div className={styles.Description__imageContainer}>
+        <Image
+          src="/images/description.jpeg"
+          alt="products marketplace"
+          fill
+          loading="eager"
+        />
+      </div>
+
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
 
         <p>
-          Future World: Your Gateway to Tomorrow&apos;s Tech! Dive into a world of
-          cutting-edge gadgets and gear. Stay ahead of the curve and redefine
+          Future World: Your Gateway to Tomorrow&apos;s Tech! Dive into a world
+          of cutting-edge gadgets and gear. Stay ahead of the curve and redefine
           your digital lifestyle with us.
         </p>
       </div>
